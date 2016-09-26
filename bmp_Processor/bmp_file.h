8ebe872cdff69205338b17351609c1d3ad47bd84
evs2012 <evanmweiss12@gmail.com>
2016-09-26 16:42:40 -0400

    Finished my TODOs
    Made the function for scaling brightness and contrast
    Fixed Histogram Equalization to output to the appropriate vector
    Adjusted a few UI things

--- bmp_Processor/bmp_file.h
+++ bmp_Processor/bmp_file.h
@@ -10,30 +10,30 @@
 
 class bmp_file
 {    
-    bmp_file();                                                 /**< Default constructor */
-    virtual ~bmp_file();                                        /**< Default destructor */
-    bmp_file(char* filepath);                                   /**< Creates an instance of bmp_file containing the specified file */
+    bmp_file();                                                         /**< Default constructor */
+    virtual ~bmp_file();                                                /**< Default destructor */
+    bmp_file(char* filepath);                                           /**< Creates an instance of bmp_file containing the specified file */
 
-    void writeToNewFile(char* filepath);                        /**< Writes the data in the bmp_file instance to the specified file */
-    void histogram_equalization();                              /**< Histogram equalizes the image and writes it to the Output vector */
-    void imageOverlay(bmp_file overlayImage, char * outFile);   /**< Takes the overlay image as a Bmp_file object and overlays it over this bmp_file */
-    void sliderBarAdjustment(double brightness, int contrast);  /**< Takes the numerical inputs from the slider bars and edits the image */
-    unsigned long getFileSize();                                /**< Extracts the file size from the bitmap header */
-    unsigned long getStartOfBitmap();                           /**< Extracts the image offset from the bitmap header */
-    unsigned long getWidth();                                   /**< Extracts the image width from the bitmap header */
-    unsigned long getHeight();                                  /**< Extracts the image height from the bitmap header */
-    unsigned long getNumberOfColorsInPalette();                 /**< Extracts the bits/px value from the bitmap header */
-    void printData();                                           /**< Outputs header data to the console using the std::cout stream */
+    void writeToNewFile(char* filepath);                                /**< Writes the data in the bmp_file instance to the specified file */
+    void histogram_equalization();                                      /**< Histogram equalizes the image and writes it to the Output vector */
+    void imageOverlay(bmp_file overlayImage, char * outFile);           /**< Takes the overlay image as a Bmp_file object and overlays it over this bmp_file */
+    void sliderBarAdjustment(double brightness, int contrast); /**< Takes the numerical inputs from the slider bars and edits the image */
+    unsigned long getFileSize();                                        /**< Extracts the file size from the bitmap header */
+    unsigned long getStartOfBitmap();                                   /**< Extracts the image offset from the bitmap header */
+    unsigned long getWidth();                                           /**< Extracts the image width from the bitmap header */
+    unsigned long getHeight();                                          /**< Extracts the image height from the bitmap header */
+    unsigned long getNumberOfColorsInPalette();                         /**< Extracts the bits/px value from the bitmap header */
+    void printData();                                                   /**< Outputs header data to the console using the std::cout stream */
 
-    unsigned char getPixel(int index);                          /**< Returns the pixel data located in the specified file byte */
-    void setPixel(int index, unsigned char byte);               /**< Writes a byte to the specified pixel */
+    unsigned char getPixel(int index);                                  /**< Returns the pixel data located in the specified file byte */
+    void setPixel(int index, unsigned char byte);                       /**< Writes a byte to the specified pixel */
 
 private:
-    unsigned long get32(int LSBindex);                          /**< Extracts a 32-bit unsigned long value from fileData beginning with element LSBindex */
-    std::vector<unsigned char> fileData;                        /**< Contains the original bitmap data */
-    std::vector<unsigned char> IO_fileOutput;                   /**< Contains the resulting bitmap data after the IO is performed */
-    std::vector<unsigned char> HE_fileOutput;                   /**< Contains the resulting bitmap data after HE*/
-    std::vector<unsigned char> SB_fileOutput;                   /**< Contains the resulting bitmap data after Slider bars adjust contrast and brightness */
+    unsigned long get32(int LSBindex);                                  /**< Extracts a 32-bit unsigned long value from fileData beginning with element LSBindex */
+    std::vector<unsigned char> fileData;                                /**< Contains the original bitmap data */
+    std::vector<unsigned char> IO_fileOutput;                           /**< Contains the resulting bitmap data after the IO is performed */
+    std::vector<unsigned char> HE_fileOutput;                           /**< Contains the resulting bitmap data after HE*/
+    std::vector<unsigned char> SB_fileOutput;                           /**< Contains the resulting bitmap data after Slider bars adjust contrast and brightness */
 };
 
 /// used to count the occurences of colors and then calculate the new color in Histogram Equalization
--- build-bmp_Processor-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-bmp_Processor-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[221];
+    QByteArrayData data[9];
+    char stringdata0[255];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,14 +36,16 @@ QT_MOC_LITERAL(3, 43, 31), // "on_horizontalSlider_sliderMoved"
 QT_MOC_LITERAL(4, 75, 8), // "position"
 QT_MOC_LITERAL(5, 84, 44), // "on_actionSave_Image_Overlay_R..."
 QT_MOC_LITERAL(6, 129, 53), // "on_actionSave_Histogram_Equal..."
-QT_MOC_LITERAL(7, 183, 37) // "on_actionLoad_Overlay_Image_t..."
+QT_MOC_LITERAL(7, 183, 37), // "on_actionLoad_Overlay_Image_t..."
+QT_MOC_LITERAL(8, 221, 33) // "on_horizontalSlider_2_sliderM..."
 
     },
     "MainWindow\0on_actionLoad_Bitmap_triggered\0"
     "\0on_horizontalSlider_sliderMoved\0"
     "position\0on_actionSave_Image_Overlay_Result_triggered\0"
     "on_actionSave_Histogram_Equalization_Result_triggered\0"
-    "on_actionLoad_Overlay_Image_triggered"
+    "on_actionLoad_Overlay_Image_triggered\0"
+    "on_horizontalSlider_2_sliderMoved"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,7 +55,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -61,11 +63,12 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    1,   40,    2, 0x08 /* Private */,
-       5,    0,   43,    2, 0x08 /* Private */,
-       6,    0,   44,    2, 0x08 /* Private */,
-       7,    0,   45,    2, 0x08 /* Private */,
+       1,    0,   44,    2, 0x08 /* Private */,
+       3,    1,   45,    2, 0x08 /* Private */,
+       5,    0,   48,    2, 0x08 /* Private */,
+       6,    0,   49,    2, 0x08 /* Private */,
+       7,    0,   50,    2, 0x08 /* Private */,
+       8,    1,   51,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -73,6 +76,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    4,
 
        0        // eod
 };
@@ -88,6 +92,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_actionSave_Image_Overlay_Result_triggered(); break;
         case 3: _t->on_actionSave_Histogram_Equalization_Result_triggered(); break;
         case 4: _t->on_actionLoad_Overlay_Image_triggered(); break;
+        case 5: _t->on_horizontalSlider_2_sliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -118,13 +123,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 6;
     }
     return _id;
 }
